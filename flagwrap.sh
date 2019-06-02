72e4e5da11fedea8d9e68d024667494499340720
TSC <gst659@gmail.com>
2019-06-02 01:05:13 -0700

    Initial commit.

---
+++ flagwrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+declare -A replacable=( \
+  [--duplex]="" \
+  [--foo]="bar" \
+  [--x]="y" \
+)
+
+declare -A appendable=( \
+  [--broker_arg]="" \
+  [--root_arg]="" \
+  [--leaf_arg]="" \
+)
+
+function set_user_flag () {
+    IFS='=' read -r key value  <<< "$1"
+    if [[ -n "${appendable[${key}]+APPEND}" ]]; then
+        appendable["${key}"]+=" ${value}"
+    else
+        replacable["${key}"]="${value}"
+    fi
+}
+
+function main() {
+    for kv in "$@"; do
+        set_user_flag "${kv}"
+    done
+    local -a argv=( "target" )
+    for key in "${!replacable[@]}"; do
+        local value="${replacable[$key]}"
+        if [[ -n "${value}" ]]; then
+            argv+=( "${key}=${value}" )
+        else
+            argv+=( "${key}" )
+        fi
+    done
+    for key in "${!appendable[@]}"; do
+        for value in ${appendable["${key}"]}; do
+            argv+=( "${key}=${value}")
+        done
+    done
+    for a in "${argv[@]}"; do
+        echo "  " ${a} "\\"
+    done
+}
+
+main "$@"
